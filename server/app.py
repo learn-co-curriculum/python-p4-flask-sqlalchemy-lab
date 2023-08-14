0129aa489e2555e4b12c6400669c9bed1ded4412
MarkTocino <Mark_tocino@yahoo.com>
2023-08-14 13:36:51 -0400

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,50 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    # looking through Zookeeper.id to find the same ID in Animal
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == animal.zookeeper_id).first()
+    enclosure = Enclosure.query.filter(Enclosure.id == animal.enclosure_id).first()
+    response_body =  f'''<ul>{animal.id}
+    <ul>Name: {animal.name}</ul>
+    <ul>Species: {animal.species}</ul>
+    <ul>Zookeeper Name:{zookeeper.name}</ul>
+    <ul>Enclosure: {enclosure.environment}</ul>'''
+    if not animal:
+        response_body = "<h1>404 No Animals Found</h1>"
+        response = make_response(response_body,404)
+        return response
+    
+    response = make_response(response_body,200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    response_body = f'<ul>Name:{zookeeper.name}</ul>'
+    response_body += f'<ul>Birthday: {zookeeper.birthday}</ul>'
+    animals = [pet for pet in zookeeper.animals]
+    if not animals:
+        response_body += f'<ul>No animals</ul>'
+    else:
+        for animal in zookeeper.animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    response_body = f'<ul>Environment:{enclosure.environment}</ul>'
+    response_body += f'<ul>Open to Visitors:{enclosure.open_to_visitors}</ul>'
+    animals = [pet for pet in enclosure.animals]
+    if not animals:
+        response_body += '<ul>No animals in this environment</ul>'
+    else:
+        for pet in enclosure.animals:
+            response_body += f'<ul>Animal: {pet.name}</ul>'
+    response = make_response(response_body, 200)
+    return response
 
 
 if __name__ == '__main__':
